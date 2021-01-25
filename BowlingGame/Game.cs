8f12d37c9aa04f63aa6ff15d53055b6cec1128ee
Xin MA <xin.ma@axa.fr>
2021-01-25 02:16:49 +0100

    feat: add strike case & refactoring

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -3,14 +3,28 @@
     public class Game
     {
         private int Score { get; set; }
+        private int[] Rolls { get; set; } = new int[21];
+        private int CurrentIndex { get; set; }
 
         public void Roll(int nbPins)
         {
-            Score += nbPins;
+            Rolls[CurrentIndex] = nbPins;
+            CurrentIndex++;
         }
 
         public int CalculateScore()
         {
+            for(int i=0; i<Rolls.Length; i++)
+            {
+                if (Rolls[i] == 10)
+                {
+                    Score += (10 + Rolls[i + 1] + Rolls[i + 2]);
+                }
+                else
+                {
+                    Score += Rolls[i];
+                }
+            }
             return Score;
         }
     }
--- BowlingGameTest/BowlingGameShould.cs
+++ BowlingGameTest/BowlingGameShould.cs
@@ -1,30 +1,43 @@
 ﻿using BowlingGame;
 using NUnit.Framework;
-
 namespace BowlingGameTest
 {
+    [TestFixture]
     public class BowlingGameShould
     {
         [Test]
         public void Return_0_when_no_pins_striked_down()
         {
-            var game = new Game();
-            for (int i = 1; i <= 20; i++)
-            {
-                game.Roll(0);
-            }
+            Game game = new Game();
+            RollForMany(20, 0, game);
             Assert.AreEqual(0, game.CalculateScore());
         }
 
         [Test]
         public void Return_20_when_all_one_pin_striked_down()
         {
-            var game = new Game();
-            for (int i = 1; i <= 20; i++)
-            {
-                game.Roll(1);
-            }
+            Game game = new Game();
+            RollForMany(20, 1, game);
             Assert.AreEqual(20, game.CalculateScore());
+        }
+
+        [Test]
+        public void Return_correct_score_with_one_strike()
+        {
+            Game game = new Game();
+            game.Roll(10);
+            game.Roll(1);
+            game.Roll(1);
+            RollForMany(16, 0, game);
+            Assert.AreEqual(14, game.CalculateScore());
+        }
+
+        private void RollForMany(int nbRoll, int nbPinsPerRoll, Game game)
+        {
+            for (int i = 1; i <= nbRoll; i++)
+            {
+                game.Roll(nbPinsPerRoll);
+            }
         }
     }
 }
