4eb32b03e651a369923a0552fbe50b87996c7f94
Xin MA <xin.ma@axa.fr>
2021-01-25 02:23:11 +0100

    refactor: use setup to initialize test

--- BowlingGameTest/BowlingGameShould.cs
+++ BowlingGameTest/BowlingGameShould.cs
@@ -5,38 +5,42 @@ namespace BowlingGameTest
     [TestFixture]
     public class BowlingGameShould
     {
+        private Game _game;
+        [SetUp]
+        public void InitTest()
+        {
+            _game = new Game();
+        }
+
         [Test]
         public void Return_0_when_no_pins_striked_down()
         {
-            Game game = new Game();
-            RollForMany(20, 0, game);
-            Assert.AreEqual(0, game.CalculateScore());
+            RollForMany(20, 0);
+            Assert.AreEqual(0, _game.CalculateScore());
         }
 
         [Test]
         public void Return_20_when_all_one_pin_striked_down()
         {
-            Game game = new Game();
-            RollForMany(20, 1, game);
-            Assert.AreEqual(20, game.CalculateScore());
+            RollForMany(20, 1);
+            Assert.AreEqual(20, _game.CalculateScore());
         }
 
         [Test]
         public void Return_correct_score_with_one_strike()
         {
-            Game game = new Game();
-            game.Roll(10);
-            game.Roll(1);
-            game.Roll(1);
-            RollForMany(16, 0, game);
-            Assert.AreEqual(14, game.CalculateScore());
+            _game.Roll(10);
+            _game.Roll(1);
+            _game.Roll(1);
+            RollForMany(16, 0);
+            Assert.AreEqual(14, _game.CalculateScore());
         }
 
-        private void RollForMany(int nbRoll, int nbPinsPerRoll, Game game)
+        private void RollForMany(int nbRoll, int nbPinsPerRoll)
         {
             for (int i = 1; i <= nbRoll; i++)
             {
-                game.Roll(nbPinsPerRoll);
+                _game.Roll(nbPinsPerRoll);
             }
         }
     }
